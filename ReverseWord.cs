6faf50d70f8766def811edad3416d065b1613197
JangYuseong <dog2655@naver.com>
2025-05-09 19:52:49 +0900

    feat: 백준9093 단어뒤집기

---
+++ ReverseWord.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace CodingTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int testCaseCount = int.Parse(Console.ReadLine());
+        int wordLength;
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        for(int i=0; i<testCaseCount; i++)
+        {
+            string inputString=Console.ReadLine();
+            string[] words = inputString.Split(" ");
+
+            foreach(string word in words)
+            {
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                string reversed = new string(charArray);
+                stringBuilder.Append(reversed+" ");
+            }
+            stringBuilder.Append("\n");
+        }
+
+        Console.WriteLine(stringBuilder);
+    }
+}
