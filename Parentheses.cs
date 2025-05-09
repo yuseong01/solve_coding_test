ff2f3fdd3fd64c7a665adf5f4ba02963197bdf23
JangYuseong <dog2655@naver.com>
2025-05-09 19:57:19 +0900

    feat: 백준9012 괄호

---
+++ Parentheses.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace CodingTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        num=int.Parse(Console.ReadLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        for(int i=0; i<num; i++)
+        {
+            char[] inputString = Console.ReadLine().ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            
+            foreach(char input in inputString)
+            {
+                if(input=='(')
+                    stack.Push(input);
+                else if(input==')')
+                {
+                    if(stack.Count>0&&stack.Peek()=='(')
+                        stack.Pop();
+                    else
+                        stack.Push(input);
+                }
+            }
+            if(stack.Count==0)
+                stringBuilder.AppendLine("YES");
+            else
+                stringBuilder.AppendLine("NO");
+        }
+        Console.WriteLine(stringBuilder);
+    }
+}
