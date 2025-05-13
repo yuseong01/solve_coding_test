90ddcc450ecfc94a1508b4f9b848e3051e1de6d1
JangYuseong <dog2655@naver.com>
2025-05-14 00:14:47 +0900

    feat: 백준10866 덱

---
+++ Deque.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace CodingTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        int front=-1;
+        StringBuilder sb = new StringBuilder();
+
+        int N=int.Parse(Console.ReadLine());
+    
+        for(int i=0; i<N; i++)
+        {
+            string[] commend = Console.ReadLine().Split(" ");
+            switch(commend[0])
+            {
+                case "push_front":
+                    list.Insert(0, commend[1]);
+                    break;
+                case "push_back":
+                    list.Add(commend[1]);
+                    break;
+                case "pop_front":
+                    sb.AppendLine(list.Count==0? "-1":list[0]);
+                    if(list.Count!=0) list.RemoveAt(0);
+                    break;
+                case "pop_back":
+                    sb.AppendLine(list.Count==0? "-1": list[list.Count-1]);
+                    if(list.Count!=0) list.RemoveAt(list.Count-1);
+                    break;
+                case "size":
+                    sb.AppendLine(list.Count==0? "0":list.Count().ToString());
+                    break;
+                case "empty":
+                    sb.AppendLine(list.Count==0? "1":"0");
+                    break;
+                case "front":
+                    sb.AppendLine(list.Count==0? "-1": list[0]);
+                    break;
+                case "back":
+                    sb.AppendLine(list.Count==0? "-1": list[list.Count-1]);
+                    break;
+            }
+
+        }
+        Console.WriteLine(sb);
+    }
+}
