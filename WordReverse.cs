ec451f25b22e5d03cb032b6548c35a4689a259bb
JangYuseong <dog2655@naver.com>
2025-05-20 19:27:26 +0900

    feat: 백준17413 단어 뒤집기2

---
+++ WordReverse.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.Versioning;
+using System.Text;
+
+namespace CodingTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            if (inputString[i] == '<')
+            {
+                while (stack.Count() != 0) sb.Append(stack.Pop());
+                while (inputString[i] != '>')
+                {
+                    sb.Append(inputString[i]);
+                    i++;
+                }
+                sb.Append(inputString[i]);
+            }
+            else if (inputString[i] == ' ')
+            {
+                while (stack.Count() != 0) sb.Append(stack.Pop());
+                sb.Append(inputString[i]);
+            }    
+            else
+            {
+                stack.Push(inputString[i]);
+            }
+        }
+        while (stack.Count() != 0) sb.Append(stack.Pop());
+
+        Console.Write(sb);
+    }
+}
